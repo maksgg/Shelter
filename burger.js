c0eefabb87725e92e97066a36abc3004a0ba28fc
maksgg <maks.zhovmir.97@gmail.com>
2024-09-20 18:04:15 +0300

    feat: rewrite burger drop function and remove coments

--- burger.js
+++ burger.js
@@ -1,47 +1,11 @@
 const burgerBtn = document.querySelector(".burger-menu-icon");
-
-const closeBurgerMenu = document.querySelector(".burger-menu");
+const burgerMenu = document.querySelector(".burger-menu");
 const body = document.querySelector("body");
 const navLinks = document.querySelectorAll(".list__item a");
 const overlay = document.querySelector('.overlay');
 
 //burger animation
 burgerBtn.addEventListener('click', () => {
-
-
-  
-  body.classList.toggle('lock-scroll');
   burgerBtn.classList.toggle('rotate');
-  closeBurgerMenu.classList.toggle('active');
-  closeBurgerMenu.classList.toggle('hidden-pets');
-  overlay.style.display = closeBurgerMenu.classList.contains('active') ? 'block' : 'none';
+  burgerMenu.classList.toggle('active');
 })
-
-document.addEventListener('click', function(event) {
-  const isClickInside = closeBurgerMenu.contains(event.target) || burgerBtn.contains(event.target);
-  
-  if (!isClickInside) {
-    closeBurgerMenu.classList.remove('active');
-    burgerBtn.classList.remove('rotate');
-    closeBurgerMenu.classList.remove('hidden-pets');
-    body.classList.remove('lock-scroll');
-    overlay.style.display = 'none'; // hide overlay
-  }
-});
-overlay.addEventListener('click', function() {
-  closeBurgerMenu.classList.remove('active');
-  /* closeBurgerMenu.classList.remove('hidden-burger'); */
-  burgerBtn.classList.remove('active');
-  closeBurgerMenu.classList.remove('hidden-pets');
-  body.classList.remove('lock-scroll');
-  overlay.style.display = 'none'; // hide overlay
-});
-navLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-    closeBurgerMenu.classList.remove('active');
-    closeBurgerMenu.classList.remove('hidden-pets');
-    burgerBtn.classList.remove('rotate'); /// return the button to its initial state
-    body.classList.remove('lock-scroll'); // return the ability to scroll
-    overlay.style.display = 'none';
-  });
-});
--- index.js
+++ index.js
@@ -1,49 +1,5 @@
 console.log("Самооцінка: 80-90 балів");
 
-const burgerBtn = document.querySelector(".burger-menu-icon");
-
-const closeBurgerMenu = document.querySelector(".burger-menu");
-const body = document.querySelector("body");
-const navLinks = document.querySelectorAll(".list__item a");
-const overlay = document.querySelector('.overlay');
-const popupBtn = document.querySelector('.popup-close-btn');
-const closePopupMenu = document.querySelector('.popup-container');
-
-//burger animation
-burgerBtn.addEventListener('click', () => {
-  body.classList.toggle('lock-scroll');
-  burgerBtn.classList.toggle('rotate');
-  closeBurgerMenu.classList.toggle('active');
-  overlay.style.display = closeBurgerMenu.classList.contains('active') ? 'block' : 'none';
-})
-popupBtn.addEventListener('click', () => {
-    closePopupMenu.classList.toggle('active');
-})
-
-document.addEventListener('click', function(event) {
-  const isClickInside = closeBurgerMenu.contains(event.target) || burgerBtn.contains(event.target);
-  
-  if (!isClickInside) {
-    closeBurgerMenu.classList.remove('active');
-    burgerBtn.classList.remove('rotate');
-    body.classList.remove('lock-scroll');
-    overlay.style.display = 'none'; // hide overlay
-  }
-});
-overlay.addEventListener('click', function() {
-  closeBurgerMenu.classList.remove('active');
-  burgerBtn.classList.remove('active');
-  body.classList.remove('lock-scroll');
-  overlay.style.display = 'none'; // hide overlay
-});
-navLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-    closeBurgerMenu.classList.remove('active');
-    burgerBtn.classList.remove('rotate'); /// return the button to its initial state
-    body.classList.remove('lock-scroll'); // return the ability to scroll
-    overlay.style.display = 'none';
-  });
-});
 
 // Slider function
 // Отримуємо елементи
